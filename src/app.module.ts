1d210c2d58b91a008da61e6a3bf36e550059f85a
AlexShatokhin <sashatokhin.04@gmail.com>
2025-12-08 11:57:39 +0200

    open static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,18 @@ import { UserModule } from './user/user.module';
 import { StampModule } from './stamp/stamp.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
 	imports: [
+		ServeStaticModule.forRoot({
+            rootPath: join(process.cwd(), 'images'),
+            serveRoot: '/images',
+            serveStaticOptions: {
+                index: false, 
+            },
+		}),
 		CafeModule,
 		PrismaModule,
 		UserModule,
